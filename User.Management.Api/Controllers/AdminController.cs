4f335b58036186b535805232a6bf2d540878d911
Osyny <oxana1404@gmail.com>
2025-04-22 21:54:19 +0300

    Added Authorize, check Roles

---
+++ User.Management.Api/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Management.Api.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("employees")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "employee1", "employee2", "employee3" };
+        }
+    }
+}
